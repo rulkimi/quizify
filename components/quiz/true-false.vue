5b814e3cfd1b469ec8927d35a203f96f963922ce
rulkimi <azrulkimi00@gmail.com>
2024-12-29 18:20:04 +0800

    add answer mode for true false question

--- components/quiz/true-false.vue
+++ components/quiz/true-false.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 const emit = defineEmits(['remove', 'save']);
 
-defineProps<{
+const props = defineProps<{
   index: number
+  quizMode: 'edit' | 'answer'
 }>()
 
+const mode = ref(props.quizMode)
 const question = ref('');
 const type = ref('true-false');
-const answers = ref<boolean>(true);
+const answers = ref<boolean>(true); // Make sure answers is a boolean
 </script>
 
 <template>
@@ -20,27 +22,40 @@ const answers = ref<boolean>(true);
     </template>
 
     <div class="space-y-4">
-      <UFormGroup required label="Question">
+      <UFormGroup
+        :required="mode === 'edit'"
+        label="Question"
+      >
+        <div class="text-lg font-semibold" v-if="mode === 'answer'">{{ question }}</div>
         <UInput
+          v-if="mode === 'edit'"
           placeholder="Enter your question"
           v-model="question"
         />
       </UFormGroup>
 
-      <UFormGroup required label="Answers">
+      <UFormGroup
+        :required="mode === 'edit'"
+        label="Answers"
+      >
         <div class="flex gap-4">
-          <URadio
-            name="correct-answer"
-            label="True"
-            value="true"
-            v-model="answers"
-          />
-          <URadio
-            name="correct-answer"
-            label="False"
-            value="false"
-            v-model="answers"
-          />
+          <template v-if="mode === 'edit'">
+            <URadio
+              name="correct-answer"
+              label="True"
+              :value="true"
+              v-model="answers"
+            />
+            <URadio
+              name="correct-answer"
+              label="False"
+              :value="false"
+              v-model="answers"
+            />
+          </template>
+          <span v-if="mode === 'answer'">
+            {{ answers ? 'True' : 'False' }}
+          </span>
         </div>
       </UFormGroup>
     </div>
@@ -48,7 +63,8 @@ const answers = ref<boolean>(true);
     <template #footer>
       <div class="flex justify-end items-center gap-2">
         <UButton variant="link" @click="emit('remove')">Remove</UButton>
-        <UButton @click="emit('save', { type, question, answers })">Save</UButton>
+        <UButton v-if="mode === 'edit'" @click="mode = 'answer'; emit('save', { type, question, answers })">Save</UButton>
+        <UButton v-else @click="mode = 'edit'">Edit</UButton>
       </div>
     </template>
 
